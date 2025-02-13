2ffbaf5b2f7921726973699acffd9abe00f7b1e5
Karine Gomes <karine.gomes@estudante.ufjf.br>
2025-02-13 10:24:36 -0300

    Update GrafoMatriz.h
    Teste

--- include/GrafoMatriz.h
+++ include/GrafoMatriz.h
@@ -1,17 +1,16 @@
-#ifndef GRAFOMATRIZ_H
-#define GRAFOMATRIZ_H
+#ifndef GRAFO_LISTA_H
+#define GRAFO_LISTA_H
 #include "Grafo.h"
+#include "Aresta.h"
 
 class Grafo;
+class Vertice;
 
-/// Grafo_matriz - uso de matriz de adjacência para representar arestas
-/// (quando grafo não direcionado deve ser usado a representação linear de matriz triangular).
-
-class GrafoMatriz : public Grafo {
+class GrafoLista : public Grafo {
 public:
     /// 1ª Parte do Trabalho
-    GrafoMatriz();
-    ~GrafoMatriz() override;
+    GrafoLista();
+    ~GrafoLista() override;
     void setDirecao(bool dir) override;
     Vertice* getVertice(int id) override;
     Aresta* getAresta(int id_inicio, int id_fim) override;
@@ -20,16 +19,11 @@ public:
     void inserirAresta(int id_inicio, int id_fim, float peso) override;
     void removerVertice(int id) override;
     void removerAresta(int id_inicio, int id_fim) override;
+    void imprimirVertices();
     void imprimirArestas();
-    void imprimirVertices() override;
 private:
-    int MAX_VERTICES;
-    int MAX_ARESTAS;
-    Aresta **arestas; // Matriz de arestas
-    Vertice **vertices; // Vetor de vértices
-    void aumentarMatriz(); // Aumenta a matriz de adjacência quando ultrapassado MAX_VERTICES
-    int detIndice(int i, int j); // Calcula a posição na matriz linear
-    bool verificaIndice(int id); // Verifica se o indice pertence ao grafo
+    Vertice* raizVertice;
+    Aresta* raizAresta;
 };
 
-#endif
+#endif //GRAFO_LISTA_H
